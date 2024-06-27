4750b83843664ac2615093a4108f44e1d308676e
1Edum <eduardomatosdesouza2@gmail.com>
2024-06-27 13:55:23 -0300

    feat: add api

---
+++ backend/src/main/java/com/br/backend/api/FoodRestController.java
@@ -0,0 +1,25 @@
+package com.br.backend.api;
+
+import com.br.backend.model.Food;
+import com.br.backend.repository.FoodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/food")
+public class FoodRestController {
+    @Autowired
+    private FoodRepository foodRepository;
+    @GetMapping("/listar")
+        public List<Food> listar(){
+        return foodRepository.findAll();
+    }
+    @PostMapping("/inserir")
+        public  void inserir(@RequestBody Food food){
+        foodRepository.save(food);
+    }
+
+
+}
--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./_components/header";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -22,7 +21,8 @@ export default function RootLayout({
         <header>
           <Header />
         </header>
-        {children}</body>
+        {children}
+      </body>
     </html>
   );
 }
