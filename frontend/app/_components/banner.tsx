3614a8b1072a6e2451dfe54b1a98679c4159df15
1Edum <eduardomatosdesouza2@gmail.com>
2024-07-02 10:17:20 -0300

    feat: update links in banner component

--- frontend/app/_components/banner.tsx
+++ frontend/app/_components/banner.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import Button from './Button'
+import Link from 'next/link'
+import Button from './button'
 
 export default function Banner() {
   return (
@@ -8,8 +9,12 @@ export default function Banner() {
         <p>Plataforma de apoio à alimentação para alunos e
         cozinheiros do SESI.</p>
         <div className='py-4 flex flex-col gap-3'>
-        <Button text="Entrar"/>
-        <Button text="Ainda não é aluno?"/>
+          <Link href={"/login"}>
+            <Button text="Entrar"/>
+          </Link>
+          <Link href={"/cadastrar"}>
+            <Button text="Ainda não é aluno?"/>
+          </Link>
         </div>
     </div>
   )
