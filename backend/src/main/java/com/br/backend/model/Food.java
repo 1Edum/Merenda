bc85494e9a71207da0162ba704f546d9978fc085
1Edum <eduardomatosdesouza2@gmail.com>
2024-07-03 10:32:49 -0300

    feat: update Food class java

--- backend/src/main/java/com/br/backend/model/Food.java
+++ backend/src/main/java/com/br/backend/model/Food.java
@@ -16,7 +16,7 @@ public class Food {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)  // Adicionando geração automática de ID
     private Long id;
-    private String nome;
+    private String name;
     private String description;
     private String image;
     private String category;
