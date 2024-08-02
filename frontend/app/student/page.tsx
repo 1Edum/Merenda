284e7a0bb5caf479ef7d87741cf1b71560121333
1Edum <eduardomatosdesouza2@gmail.com>
2024-08-02 19:34:28 -0300

    feat: update page student

--- frontend/app/student/page.tsx
+++ frontend/app/student/page.tsx
@@ -3,13 +3,13 @@ import FoodItem from '../_components/food-item'
 
 function page() {
   return (
-    <div  className='p-2'>
+    <div className='p-2 flex flex-col gap-y-2'>
       <h3>Café da Manhã</h3>
-        <FoodItem category='cafe-manha' />
+        <FoodItem bg='bg-green-400' text='Quero' category='cafe-manha' />
       <h3>Almoço</h3>
-        <FoodItem category='almoco' />
+        <FoodItem bg='bg-green-400' text='Quero' category='almoco' />
       <h3>Café da Tarde</h3>
-        <FoodItem category='cafe-tarde' />
+        <FoodItem bg='bg-green-400' text='Quero' category='cafe-tarde' />
 
 
     </div>
