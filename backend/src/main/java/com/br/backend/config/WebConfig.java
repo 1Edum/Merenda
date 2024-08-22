29824a4587ca47d44ba7a106948a4fb16e885471
1Edum <eduardomatosdesouza2@gmail.com>
2024-08-22 15:25:04 -0300

    feat: create WebConfig for CrossOrigin global

---
+++ backend/src/main/java/com/br/backend/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.br.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Permite todas as URLs
+                .allowedOrigins("http://localhost:3000")  // Define a origem permitida
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Permite todos os métodos
+                .allowedHeaders("*")  // Permite todos os cabeçalhos
+                .allowCredentials(true);  // Permite credenciais, se necessário
+    }
+}
