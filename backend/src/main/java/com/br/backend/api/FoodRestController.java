e603796b8e8b579c6a56db3ff00e8d4a9faf0ff0
1Edum <eduardomatosdesouza2@gmail.com>
2024-08-22 15:20:44 -0300

    feat: update in CrossOrigin FoodRestController

--- backend/src/main/java/com/br/backend/api/FoodRestController.java
+++ backend/src/main/java/com/br/backend/api/FoodRestController.java
@@ -3,24 +3,37 @@ package com.br.backend.api;
 import com.br.backend.model.Food;
 import com.br.backend.repository.FoodRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/food")
 public class FoodRestController {
     @Autowired
     private FoodRepository foodRepository;
-    @CrossOrigin(origins = "http://localhost:3000")
+
     @GetMapping("/listar")
-        public List<Food> listar(){
+    public List<Food> listar() {
         return foodRepository.findAll();
     }
+
     @PostMapping("/inserir")
-        public  void inserir(@RequestBody Food food){
+    public void inserir(@RequestBody Food food) {
         foodRepository.save(food);
     }
 
-
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable("id") Long id) {
+        Optional<Food> existingFood = foodRepository.findById(id);
+        if (existingFood.isPresent()) {
+            foodRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
