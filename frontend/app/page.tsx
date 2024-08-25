3faf3172088405e8dafac43a4d7e936ea27328f4
1Edum <eduardomatosdesouza2@gmail.com>
2024-08-25 16:24:09 -0300

    feat: updating page initial

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,14 @@
 import Banner from "./_components/banner";
+import Header from "./_components/header";
 
 
 export default function Home() {
   return (
+    <>
+    <Header />
     <main>
       <Banner />
     </main>
+    </>
   );
 }
