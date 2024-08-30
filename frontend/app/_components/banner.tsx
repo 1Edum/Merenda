9687d27ed94768e977290e8d4baf0e9faa40d06a
1Edum <eduardomatosdesouza2@gmail.com>
2024-08-30 10:03:09 -0300

    feat: add banner shadcn-ui component and config initial for variables

--- frontend/app/_components/banner.tsx
+++ frontend/app/_components/banner.tsx
@@ -1,22 +1,31 @@
 import React from "react";
 import Link from "next/link";
-import Button from "./button";
+import { Button } from "./ui/button";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "./ui/card";
 
 export default function Banner() {
   return (
-    <div className="bg-secondary text-center p-5 flex flex-col items-center">
-      <h1 className="text-4xl uppercase font-semibold py-3">Sesi Merenda</h1>
-      <p>
-        Plataforma de apoio à alimentação para alunos e cozinheiros do SESI.
-      </p>
-      <div className="py-4 flex flex-col gap-3 md:w-96 w-full justify-center">
+    <Card className="bg-secondary text-center p-5 flex flex-col items-center">
+      <CardHeader>
+        <CardTitle className="text-4xl uppercase font-semibold py-3">Sesi Merenda</CardTitle>
+        <CardDescription>
+          Plataforma de apoio à alimentação para alunos e cozinheiros do SESI.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-3 w-64">
         <Link href={"/login"}>
           <Button>Entrar </Button>
         </Link>
         <Link href={"/cadastrar"}>
           <Button>Não é Aluno ainda? </Button>
         </Link>
-      </div>
-    </div>
+      </CardContent>
+    </Card>
   );
 }
