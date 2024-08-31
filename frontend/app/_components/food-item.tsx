9019615dcc635a2fd2c06e3e8ff0270aa4386455
1Edum <eduardomatosdesouza2@gmail.com>
2024-08-31 20:50:30 -0300

    feat: add card component and updating in styles

--- frontend/app/_components/food-item.tsx
+++ frontend/app/_components/food-item.tsx
@@ -4,6 +4,7 @@
 import React from 'react';
 import Image from 'next/image';
 import useFetchFoods from '../_hooks/useFetchFoods';
+import { Card, CardContent, CardHeader, CardDescription, CardTitle } from './ui/card';
 
 interface FoodItemProps {
   category?: string;
@@ -33,28 +34,29 @@ const FoodItem: React.FC<FoodItemProps> = ({ category, bg, text }) => {
     : foods;
 
   return (
-    <ul className="grid grid-cols-2 md:grid-cols-4 gap-4 ">
-      {filteredFoods.map(food => (
-        <li key={food.id} className="bg-zinc-400 h-64 md:h-80 text-white p-2 rounded-md">
-          <div className="w-full h-1/3 md:h-2/3">
-            <Image 
-              src={food.image}
-              alt={food.name}
-              className="dark:invert w-full h-full object-cover rounded-md"
-              width={100}
-              height={20}
-              priority
-              quality={100}
-            />
-          </div>
-          <div className="h-2/3 md:h-1/3 flex flex-col">
-            <h2 className="text-xl h-20">{food.name}</h2>
-            <p className="text-xs break-words h-36">{food.description}</p>
-            <button className={`${bg} w-full rounded-md`}>{text}</button>
-          </div>
-        </li>
-      ))}
-    </ul>
+<div className="grid grid-cols-2  md:grid-cols-4 gap-4">
+  {filteredFoods.map(food => (
+    <Card key={food.id} className="bg-zinc-400 h-64 md:h-80 text-white p-2 rounded-md flex flex-col">
+      <CardContent className="w-full">
+        <Image 
+          src={food.image}
+          alt={food.name}
+          className="w-full h-28 sm:h-36 md:h-48 bg-red-800 rounded-md"
+          width={100}
+          height={20}
+          priority
+          quality={100}
+        />
+      </CardContent>
+      <CardHeader className="flex flex-col h-full ">
+        <CardTitle className="text-xl">{food.name}</CardTitle>
+        <button className={`${bg} rounded-md mt-auto`}>{text}</button>
+      </CardHeader>
+    </Card>
+  ))}
+</div>
+
+
   );
 };
 
