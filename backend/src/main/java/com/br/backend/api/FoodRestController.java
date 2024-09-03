f26d9bf2b020b61bca4658cf395b1b5a0eef7d18
1Edum <eduardomatosdesouza2@gmail.com>
2024-09-03 15:01:10 -0300

    feat: add firebase

--- backend/src/main/java/com/br/backend/api/FoodRestController.java
+++ backend/src/main/java/com/br/backend/api/FoodRestController.java
@@ -2,13 +2,13 @@ package com.br.backend.api;
 
 import com.br.backend.model.Food;
 import com.br.backend.repository.FoodRepository;
+import com.br.backend.service.FirebaseStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
@@ -16,8 +16,12 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/food")
 public class FoodRestController {
+
     @Autowired
     private FoodRepository foodRepository;
+
+    @Autowired
+    private FirebaseStorageService firebaseStorageService;
 
     @GetMapping("/listar")
     public List<Food> listar() {
@@ -27,17 +31,11 @@ public class FoodRestController {
     @PostMapping("/inserir")
     public ResponseEntity<Food> inserir(@RequestPart("food") Food food, @RequestPart("image") MultipartFile image) {
         try {
-            // Defina o diretório onde o arquivo será salvo
-            String directory = "uploads/";
-            String fileName = image.getOriginalFilename();
-            String filePath = directory + fileName;
-
-            // Salve o arquivo no servidor
-            File dest = new File(filePath);
-            image.transferTo(dest);
+            // Faça o upload da imagem para o Firebase e obtenha a URL pública
+            String imageUrl = firebaseStorageService.uploadFile(image);
 
             // Defina a URL da imagem na entidade Food
-            food.setImageUrl("/uploads/" + fileName);
+            food.setImageUrl(imageUrl);
 
             // Salve a entidade Food no banco de dados
             Food savedFood = foodRepository.save(food);
@@ -48,9 +46,10 @@ public class FoodRestController {
         }
     }
 
-    // Inserir vários alunos
     @PostMapping("/inserir-varios")
-    public void inserirVarios(@RequestBody List<Food> alunos) {foodRepository.saveAll(alunos);}
+    public void inserirVarios(@RequestBody List<Food> foods) {
+        foodRepository.saveAll(foods);
+    }
 
     @DeleteMapping("/deletar/{id}")
     public ResponseEntity<Void> deletar(@PathVariable("id") Long id) {
