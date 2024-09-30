a3ff6322ec94af276b400688a85029ff0c8fd17a
1Edum <eduardomatosdesouza2@gmail.com>
2024-09-30 20:11:30 -0300

    feat: updating in page login

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,15 +1,21 @@
+import { Button } from '@/app/_components/ui/button'
+import { Input } from '@/app/_components/ui/input'
 import React from 'react'
-import { Input } from '../_components/ui/input'
-import { Button } from '../_components/ui/button'
 
 function page() {
   return (
     <main className='flex justify-center items-center w-full h-[80vh]'>
-        <div className='bg-secondary px-4 py-7 flex flex-col gap-8 w-72 md:w-96'>
+        <div className='border rounded-lg px-4 py-7 flex flex-col gap-8 w-72 md:w-96'>
             <h1 className='text-5xl text-center'>Login</h1>
-            <Input placeholder='Email'/>
-            <Input placeholder='Senha' />
-            <Button>Entrar</Button>
+            <div>
+              <label>Email</label>
+              <Input />
+            </div>
+            <div>
+              <label>Senha</label>
+              <Input />
+            </div>
+            <Button variant={"destructive"}>Entrar</Button>
         </div>
     </main>
   )
