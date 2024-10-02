bb741b14a9b12d9a0a98795b99654fa8f9d2d617
1Edum <eduardomatosdesouza2@gmail.com>
2024-10-02 19:27:03 -0300

    feat: updating in Respositorys

--- backend/src/main/java/com/br/backend/repository/ClassRoomRepository.java
+++ backend/src/main/java/com/br/backend/repository/ClassRoomRepository.java
@@ -1,2 +1,10 @@
-package com.br.backend.repository;public interface ClassRoomRepository {
+package com.br.backend.repository;
+
+import com.br.backend.model.ClassRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClassRoomRepository extends JpaRepository<ClassRoom, Long> {
+    ClassRoom findByName(String name);
 }
--- backend/src/main/java/com/br/backend/repository/RoleRepository.java
+++ backend/src/main/java/com/br/backend/repository/RoleRepository.java
@@ -1,2 +1,9 @@
-package com.br.backend.repository;public interface RoleRepository {
+package com.br.backend.repository;
+
+
+import com.br.backend.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository  extends JpaRepository<Role, Long> {
+    Role findByName(String name);
 }
