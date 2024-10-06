28a5646da5e53eaa94a42372f9dcd1491d05f2f8
1Edum <eduardomatosdesouza2@gmail.com>
2024-10-05 21:27:54 -0300

    feat: updating in User model

--- backend/src/main/java/com/br/backend/model/User.java
+++ backend/src/main/java/com/br/backend/model/User.java
@@ -1,41 +1,82 @@
 package com.br.backend.model;
 
-import java.util.Collection;
-import java.util.List;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", unique=true)
     private Long id;
 
-    private String name;
+    private String username;
+
     private String email;
+
     private String password;
 
-    // Relacionamento com Role
-    @ManyToMany(cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(
-            name = "user_roles",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id")
-    )
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id",
+                    referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn
+                    (name = "role_id",
+                            referencedColumnName = "id"))
     private Collection<Role> roles;
 
-    // Relacionamento com ClassRoom
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(
-            name = "user_classrooms",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "classroom_id")
-    )
-    private List<ClassRoom> classRoom;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        for(Role role: roles) {
+            SimpleGrantedAuthority authority = new SimpleGrantedAuthority(role.getName());
+            authorityList.add(authority);
+        }
+        return authorityList;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
