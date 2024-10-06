3d0065d0c866f9d38884a1408809e96736b31fef
1Edum <eduardomatosdesouza2@gmail.com>
2024-10-05 21:24:56 -0300

    feat: create controllers Auth and Simple

---
+++ backend/src/main/java/com/br/backend/api/SimpleController.java
@@ -0,0 +1,24 @@
+package com.br.backend.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class SimpleController {
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public ResponseEntity<String> helloAdmin(){
+        return ResponseEntity.ok("Hello Admin");
+    }
+
+    @PreAuthorize("hasRole('STUDENT')")
+    @GetMapping("/student")
+    public ResponseEntity<String> helloUser(){
+        return ResponseEntity.ok("Hello User");
+    }
+}
