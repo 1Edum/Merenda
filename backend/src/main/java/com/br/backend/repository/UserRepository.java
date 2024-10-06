a61f5bc18375dd26b819503040ec0a8e3aee31f7
1Edum <eduardomatosdesouza2@gmail.com>
2024-10-05 21:28:43 -0300

    feat: updating in User repository

--- backend/src/main/java/com/br/backend/repository/UserRepository.java
+++ backend/src/main/java/com/br/backend/repository/UserRepository.java
@@ -5,7 +5,13 @@ import org.springframework.stereotype.Repository;
 
 import com.br.backend.model.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+    Optional<User> findByUsername(String username);
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
 
 }
