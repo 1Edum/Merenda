7dec9287cff5f3c4dbd4564ee52630a0369f050e
1Edum <eduardomatosdesouza2@gmail.com>
2024-10-13 13:20:34 -0300

    feat: updating styles mobile in page admin

--- frontend/app/admin/layout.tsx
+++ frontend/app/admin/layout.tsx
@@ -10,9 +10,9 @@ export default function RootLayout({
       <body>
         <header>
           <Header menu={true} links={[
-            { name: "Adicionar Usuário", link: "#adicionar-usuário" },
-            { name: "Adicionar Alimento", link: "/admin/inserir" },
-            { name: "Cadastrar Alimento", link: "/" },
+            { name: "Adicionar Usuário", link: "#adicionarusuario" },
+            { name: "Adicionar Alimento", link: "/admin/#adicionarcomida" },
+            { name: "Tela TV", link: "/admin/telagrande" },
             ]}/>
         </header>
           {children}
--- frontend/app/admin/page.tsx
+++ frontend/app/admin/page.tsx
@@ -7,78 +7,26 @@ import { User } from "../interface/User";
 import ScrollToTopButton from "../_components/scroll-button";
 import { Food } from "../interface/Food";
 import { Table } from "../_components/table-infos/table";
-import { Trash2 } from "lucide-react";
-import Image from "next/image";
+import { CheckCircle, Trash2, XCircle } from "lucide-react";
+import {
+  deleteFood,
+  fetchFoods,
+  toggleActiveFood,
+} from "@/lib/services/food/foodService";
+import { fetchUsers, handleExcluirUser } from "@/lib/services/user/userService";
+import { TableMobile } from "@/lib/help-mobile/table-mobile";
 
 export default function Page() {
   const [users, setUsers] = useState<User[]>([]);
-  const [foods, setFoods] = useState<Food[]>([]);
   const [userFilter, setUserFilter] = useState("");
+
   const [foodFilter, setFoodFilter] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [foods, setFoods] = useState<Food[]>([]);
+  const isMobile = TableMobile();
 
   // Fetch de usuários
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await fetch("http://localhost/user/listar");
-        const data = await response.json();
-        setUsers(data);
-      } catch (error) {
-        console.error("Erro ao buscar os usuários:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchUsers();
-  }, []);
-
-  const handleExcluirUser = async (id: number) => {
-    try {
-      const response = await fetch(`http://localhost/user/deletar/${id}`, {
-        method: "DELETE",
-      });
-      if (response.ok) {
-        setUsers((prevUsers) => prevUsers.filter((user) => user.id !== id));
-        alert("Usuário excluído com sucesso!");
-      } else {
-        console.error("Erro ao excluir o usuário:", response.statusText);
-      }
-    } catch (error) {
-      console.error("Erro ao excluir o usuário:", error);
-    }
-  };
-
-  const handleExcluirFood = async (id: number) => {
-    try {
-      const response = await fetch(`http://localhost/food/deletar/${id}`, {
-        method: "DELETE",
-      });
-      if (response.ok) {
-        setFoods((prevFoods) => prevFoods.filter((food) => food.id !== id)); // Corrigido para setFoods
-        alert("Alimento excluído com sucesso!");
-      } else {
-        console.error("Erro ao excluir o alimento:", response.statusText);
-      }
-    } catch (error) {
-      console.error("Erro ao excluir o alimento:", error);
-    }
-  };
-
-  // Fetch de alimentos
-  useEffect(() => {
-    const fetchFoods = async () => {
-      try {
-        const response = await fetch("http://localhost/food/listar");
-        const data = await response.json();
-        setFoods(data);
-      } catch (error) {
-        console.error("Erro ao buscar os alimentos:", error);
-      }
-    };
-
-    fetchFoods();
+    fetchUsers(setUsers); // Chama a função para buscar usuários
   }, []);
 
   // Filtro de usuários
@@ -90,28 +38,45 @@ export default function Page() {
     user.username?.includes(userFilter)
   );
 
-  // Filtro de alimentos
+  //Parte Foods
+  useEffect(() => {
+    const loadFoods = async () => {
+      try {
+        const fetchedFoods = await fetchFoods(); // Função que busca os alimentos
+        setFoods(fetchedFoods); // Define os alimentos no estado
+      } catch (error) {
+        console.error("Erro ao carregar os alimentos:", error);
+      }
+    };
+
+    loadFoods(); // Chama a função ao carregar o componente
+  }, []);
+
+  const filteredFoods = foods.filter((food) => food.name.includes(foodFilter));
+
+  const excluirAlimento = (id: number) => {
+    deleteFood(id, setFoods);
+  };
+
+  const handleToggleActive = async (id: number, isActive: boolean) => {
+    try {
+      await toggleActiveFood(id, !isActive); // Função que altera o status de ativo
+      const updatedFoods = await fetchFoods(); // Atualiza a lista de alimentos
+      setFoods(updatedFoods);
+    } catch (error) {
+      console.error("Erro ao alterar o status de ativo:", error);
+    }
+  };
+
   const handleFilterChangeFood = (filterValue: string) => {
     setFoodFilter(filterValue);
   };
 
-  
-  const filteredFoods = foods.filter((food) => food.name.includes(foodFilter));
-
-  const titles = [
-    { name: "Imagem" },
-    { name: "Nome" },
-    { name: "Categoria" },
-    { name: "Calorias (100g)" },
-    { name: "Valor Nutricional" },
-    { name: "Ações" },
-  ];
-
   return (
     <div className="lg:px-24">
       {/* Seção de usuários */}
-      <div className="border rounded-lg my-7" id="adicionarusuario">
-        <div className="flex justify-between pb-7">
+      <div className="border rounded-lg my-7" id="adicionarcomida">
+        <div className="flex flex-col md:flex-row justify-between pb-4 space-y-2 md:space-y-0">
           <Filter
             textfilter="Filtrar usuários"
             onFilterChange={handleFilterChangeUser}
@@ -133,12 +98,12 @@ export default function Page() {
             ]}
           />
         </div>
-        {loading ? (
-          <p>Carregando usuários...</p>
-        ) : (
           <Table.Root>
             <Table.Header>
-              {["Nome", "Email", "Profissão", "Ações"].map((item) => (
+              {(isMobile
+                ? ["Nome", "Profissão", "Ações"]
+                : ["Nome", "Email", "Profissão", "Ações"]
+              ).map((item) => (
                 <Table.Cell key={item} textcell={item} />
               ))}
             </Table.Header>
@@ -146,26 +111,23 @@ export default function Page() {
               {filteredUsers.map((user) => (
                 <Table.Row key={user.id}>
                   <Table.Cell textcell={user.username} />
-                  <Table.Cell textcell={user.email} />
+                  {!isMobile && <Table.Cell textcell={user.email} />}
                   <Table.Cell
                     textcell={user.roles.map((role) => role.name).join(", ")}
                   />
-                  <Table.Actions>
-                    <Table.Action
-                      icon={Trash2}
-                      onClick={() => handleExcluirUser(user.id)}
-                    />
-                  </Table.Actions>
+                  <Table.Action
+                    icon={Trash2}
+                    onClick={() => handleExcluirUser(user.id, setUsers)}
+                  />
                 </Table.Row>
               ))}
             </Table.Body>
           </Table.Root>
-        )}
       </div>
 
-      {/* Seção de alimentos*/} 
+      {/* Seção de alimentos*/}
       <div className="border rounded-lg my-7" id="adicionarcomida">
-        <div className="flex justify-between pb-7">
+        <div className="flex flex-col md:flex-row justify-between pb-4 space-y-2 md:space-y-0">
           <Filter
             textfilter="Filtrar alimentos"
             onFilterChange={handleFilterChangeFood}
@@ -175,48 +137,74 @@ export default function Page() {
             descriptioninfo="Preencha as informações do alimento"
             apiEndpoint="http://localhost/food/inserir"
             fields={[
-              { type: 'text', name: 'imageUrl', placeholder: 'URL da Imagem' },
-              { type: 'text', name: 'name', placeholder: 'Nome do Alimento' },
-              { type: 'text', name: 'category', placeholder: 'Categoria' },
-              { type: 'number', name: 'calories', placeholder: 'Calorias' },
-              { type: 'text', name: 'nutritionalValue', placeholder: 'Valor Nutricional' },
+              { type: "text", name: "imageUrl", placeholder: "URL da Imagem" },
+              { type: "text", name: "name", placeholder: "Nome do Alimento" },
+              {
+                type: "select",
+                name: "category",
+                placeholder: "Selecione a Categoria",
+                options: ["líquido", "sólido"],
+              },
+              { type: "number", name: "calories", placeholder: "Calorias" },
+              {
+                type: "text",
+                name: "nutritionalValue",
+                placeholder: "Valor Nutricional",
+              },
             ]}
           />
         </div>
         <Table.Root>
           <Table.Header>
-            {titles.map((item) => (
-              <Table.Cell key={item.name} textcell={item.name} />
+            {(isMobile
+              ? ["Nome", "Ativo", "Quantia"]
+              : [
+                  "Imagem",
+                  "Nome",
+                  "Categoria",
+                  "Calorias",
+                  "Valor Nutricional",
+                  "Ativo",
+                  "Ações",
+                  "Quantidade"
+                ]
+            ).map((item) => (
+              <Table.Cell key={item} textcell={item} />
             ))}
           </Table.Header>
           <Table.Body>
             {filteredFoods.map((food) => (
               <Table.Row key={food.id}>
+                {!isMobile && (
                 <Table.Cell>
-                <Image
-                      src={food.imageUrl}
-                      alt={food.name}
-                      width={100}
-                      height={100}
-                    />
+                  <Table.Image src={food.imageUrl} alt={food.name} />
                 </Table.Cell>
+                )}
                 <Table.Cell textcell={food.name} />
-                <Table.Cell textcell={food.category} />
-                <Table.Cell textcell={food.calories.toString()} />
-                <Table.Cell textcell={food.nutritionalValue.toString()} />
-                <Table.Actions>
-                  <Table.Action
-                    icon={Trash2}
-                    onClick={() => handleExcluirFood(food.id)}
-                  />
-                </Table.Actions>
-               
+                {!isMobile && (
+                  <>
+                    <Table.Cell textcell={food.category} />
+                    <Table.Cell textcell={food.calories.toString()} />
+                    <Table.Cell textcell={food.nutritionalValue.toString()} />
+                  </>
+                )}
+                <Table.Action
+                  icon={food.active ? XCircle : CheckCircle}
+                  onClick={() => handleToggleActive(food.id, food.active)}
+                />
+                {!isMobile && (
+                <Table.Action
+                  icon={Trash2}
+                  onClick={() => excluirAlimento(food.id)}
+                />
+              )}
+                <Table.Cell textcell={food.amount.toString()} />
               </Table.Row>
             ))}
           </Table.Body>
         </Table.Root>
       </div>
-      
+
       <ScrollToTopButton />
     </div>
   );
