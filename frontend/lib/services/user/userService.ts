b6be13c9c5a75f6d3cc8f8a2ae867c3b38cbb76d
1Edum <eduardomatosdesouza2@gmail.com>
2024-10-14 07:42:20 -0300

    feat: updating in delete user in userService

--- frontend/lib/services/user/userService.ts
+++ frontend/lib/services/user/userService.ts
@@ -1,31 +1,34 @@
 // services/userService.ts
 import { User } from "@/app/interface/User";
 
-export const fetchUsers = async (setUsers: (users: User[]) => void, setLoading: (loading: boolean) => void) => {
+export const fetchUsers = async (setUsers: (users: User[]) => void) => {
   try {
     const response = await fetch("http://localhost/user/listar");
     const data = await response.json();
     setUsers(data);
   } catch (error) {
     console.error("Erro ao buscar os usuários:", error);
-  } finally {
-    setLoading(false);
   }
 };
 
-export const handleExcluirUser = async (id: number, setUsers: React.Dispatch<React.SetStateAction<User[]>>) => {
-    try {
-      const response = await fetch(`http://localhost/user/deletar/${id}`, {
-        method: "DELETE",
-      });
-      if (response.ok) {
-        // Garantindo que prevUsers seja do tipo User[]
-        setUsers((prevUsers: User[]) => prevUsers.filter((user) => user.id !== id));
-        alert("Usuário excluído com sucesso!");
-      } else {
-        console.error("Erro ao excluir o usuário:", response.statusText);
-      }
-    } catch (error) {
-      console.error("Erro ao excluir o usuário:", error);
+export const handleExcluirUser = async (
+  id: number,
+  setUsers: React.Dispatch<React.SetStateAction<User[]>>
+) => {
+  try {
+    const response = await fetch(`http://localhost/user/deletar/${id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      // Garantindo que prevUsers seja do tipo User[]
+      setUsers((prevUsers: User[]) =>
+        prevUsers.filter((user) => user.id !== id)
+      );
+      alert("Usuário excluído com sucesso!");
+    } else {
+      console.error("Erro ao excluir o usuário:", response.statusText);
     }
-  };
+  } catch (error) {
+    console.error("Erro ao excluir o usuário:", error);
+  }
+};
