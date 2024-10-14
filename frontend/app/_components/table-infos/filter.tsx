b6a04838cc4694f4a2c530327ccdb67128652f1b
1Edum <eduardomatosdesouza2@gmail.com>
2024-10-14 07:44:51 -0300

    feat: create style-class size-components and updating in filter and select component

--- frontend/app/_components/table-infos/filter.tsx
+++ frontend/app/_components/table-infos/filter.tsx
@@ -19,13 +19,13 @@ function Filter({textfilter, onFilterChange}: FilterProps) {
         onFilterChange(filterValue); // Chama a função para atualizar o filtro ao clicar no botão
       };
       return (
-        <div className="flex space-x-2">
+        <div className="flex space-x-2 justify-between">
           <Input
             placeholder={textfilter}
-            className="w-auto"
+            className="size-components"
             onChange={(e) => setFilterValue(e.target.value)} // Atualiza o estado do filtro
           />
-          <Button variant={"destructive"} onClick={handleFilterClick}>
+          <Button variant={"destructive"} className='h-9 md:h-10' onClick={handleFilterClick}>
             <Search className="mr-3 w-5 h-5" /> Filtrar Resultados
           </Button>
         </div>
