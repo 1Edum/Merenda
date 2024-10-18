dbf895c39b5acde942e529df40964be7725553e4
1Edum <eduardomatosdesouza2@gmail.com>
2024-10-18 12:33:19 -0300

    feat: updating domains in next-config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,64 +2,9 @@
 const nextConfig = {
     images: {
       domains: [
-        '3.bp.blogspot.com',
-        'i.pinimg.com'
+        'images.pexels.com',
       ],
     },
   };
   
   export default nextConfig;
-  
-  /*
-  [
-    {
-      "id": 1,
-      "category": "cafe-manha",
-      "description": "Pão de forma com manteiga",
-      "image": "https://3.bp.blogspot.com/-4tzM3_7T-04/WAKzMTezioI/AAAAAAAAB6c/dY8SpoE7-KMJ6-jxup8yE2dysJbTywiAwCLcB/w1200-h630-p-k-no-nu/pao-com-manteiga.jpg",
-      "name": "Pão com Manteiga"
-    },
-    {
-      "id": 2,
-      "category": "cafe-manha",
-      "description": "Ovos mexidos com temperos",
-      "image": "https://i.pinimg.com/564x/4f/db/63/4fdb631036af802c93e6c4cd983fb842.jpg",
-      "name": "Ovos Mexidos"
-    },
-    {
-      "id": 3,
-      "category": "cafe-manha",
-      "description": "Iogurte natural com granola",
-      "image": "https://i.pinimg.com/564x/b0/1a/cb/b01acb1a20e5e040993fef954a4e364b.jpg",
-      "name": "Iogurte com Granola"
-    },
-    {
-      "id": 4,
-      "category": "almoco",
-      "description": "Arroz branco com feijão, junto com frango grelado e salada de alface",
-      "image": "https://i.pinimg.com/736x/75/cf/17/75cf173208773aeffe878d1d0242f0dd.jpg",
-      "name": "Arroz, Feijão, Frango e Salada de Alface"
-    },
-    {
-      "id": 5,
-      "category": "almoco",
-      "description": "Arroz branco com feijão, junto com carne moida de e salada de alface",
-      "image": "https://i.pinimg.com/564x/c8/b2/4d/c8b24d4ea5b2573b22a1d1cc1240db45.jpg",
-      "name": "Arroz, Feijão, Carne e Salada de Alface"
-    },
-    {
-      "id": 6,
-      "category": "cafe-tarde",
-      "description": "Pão de queijo quentinho",
-      "image": "https://i.pinimg.com/564x/3b/fe/bb/3bfebb7d82b1c499f6f7b1f0e1f46a03.jpg",
-      "name": "Pão de Queijo"
-    },
-    {
-      "id": 7,
-      "category": "cafe-tarde",
-      "description": "Vitamina de banana com leite",
-      "image": "https://i.pinimg.com/564x/0e/df/10/0edf10adacf5d5c1957b8f43b3308caa.jpg",
-      "name": "Vitamina de Banana"
-    },
-]
-*/
