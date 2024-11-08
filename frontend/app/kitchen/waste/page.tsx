11c952e8042faed8eddb9180363a8ca79f2c6596
1Edum <eduardomatosdesouza2@gmail.com>
2024-11-08 09:49:39 -0300

    feat: create page-kitchen-waste

---
+++ frontend/app/kitchen/waste/page.tsx
@@ -0,0 +1,12 @@
+import WasteSection from '@/app/_components/waste-section'
+import React from 'react'
+
+function page() {
+  return (
+    <div className="lg:px-24">
+      <WasteSection />
+    </div>
+  )
+}
+
+export default page
