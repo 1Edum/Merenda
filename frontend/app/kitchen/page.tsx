a4769406d6a5ed94ff3ebe0bf137e5052ce62b46
1Edum <eduardomatosdesouza2@gmail.com>
2024-11-29 14:47:44 -0300

    feat: add waste-section in page-kitchen

--- frontend/app/kitchen/page.tsx
+++ frontend/app/kitchen/page.tsx
@@ -1,9 +1,15 @@
 import FoodSection from "../_components/food-section";
+import ScrollToTopButton from "../_components/scroll-button";
+import WasteSection from "../_components/waste-section";
 
 function Page() {
   return (
     <div className="lg:px-24">
-     <FoodSection />
+      <FoodSection />
+      <div id="waste">
+        <WasteSection />
+      </div>
+      <ScrollToTopButton />
     </div>
   );
 }
