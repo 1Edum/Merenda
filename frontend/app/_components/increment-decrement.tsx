3be84c51c9e251cf3f739612d7190d4d4890bd27
1Edum <eduardomatosdesouza2@gmail.com>
2024-12-05 21:06:22 -0300

    feat: update configs in increment-decremente component

--- frontend/app/_components/increment-decrement.tsx
+++ frontend/app/_components/increment-decrement.tsx
@@ -1,38 +1,34 @@
-import { useState } from "react";
-import { submitFoodAmount } from "@/lib/services/food/foodService"; // Importar a função de envio de quantidade
+"use client";
 
-const IncrementDecrement = ({ foodId, initialCount }: { foodId: number, initialCount: number }) => {
-  const [count, setCount] = useState(initialCount || 0);
+import { useState } from "react";
+
+interface IncrementDecrementProps {
+  foodId: number;
+  onAmountChange: (foodId: number, newAmount: number) => void;
+}
+
+const IncrementDecrement: React.FC<IncrementDecrementProps> = ({ foodId, onAmountChange }) => {
+  const [count, setCount] = useState(0);
 
   const handleIncrement = () => {
-    if (count < 2) {
+    if (count < 2) { // Verifica se o valor é menor que 2
       setCount(count + 1);
-      submitFoodAmount(foodId, count + 1); // Usar a função do serviço para enviar o valor
+      onAmountChange(foodId, count + 1); // Chama onAmountChange para atualizar o estado do pai
     }
   };
 
   const handleDecrement = () => {
-    if (count > 0) {
+    if (count > 0) { // Permite decrementar se o valor for maior que 0
       setCount(count - 1);
-      submitFoodAmount(foodId, count - 1); // Usar a função do serviço para enviar o valor
+      onAmountChange(foodId, count - 1); // Chama onAmountChange para atualizar o estado do pai
     }
   };
 
   return (
     <div className="flex justify-between border w-full rounded-full mt-2">
-      <div
-        className="w-1/3 bg-red-500 rounded-full text-center text-white cursor-pointer"
-        onClick={handleDecrement}
-      >
-        -
-      </div>
+      <div className="w-1/3 bg-red-500 rounded-full text-center text-white cursor-pointer" onClick={handleDecrement}>-</div>
       <div>{count}</div>
-      <div
-        className="w-1/3 bg-red-500 rounded-full text-center text-white cursor-pointer"
-        onClick={handleIncrement}
-      >
-        +
-      </div>
+      <div className="w-1/3 bg-red-500 rounded-full text-center text-white cursor-pointer" onClick={handleIncrement}>+</div>
     </div>
   );
 };
